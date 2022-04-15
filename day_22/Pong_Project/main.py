f95ee4ab6251054ab03db4ea7b42adcdf0aece53
Supawichza40 <supawichza@gmail.com>
2022-04-15 13:41:12 +0100

    finished the collision for the ball

--- day_22/Pong_Project/main.py
+++ day_22/Pong_Project/main.py
@@ -2,10 +2,12 @@ import time
 import turtle
 from turtle import *
 import sys
+import random
 screen = Screen()
 
 screen.bgcolor("black")
 screen.screensize(canvheight=600,canvwidth=600)
+screen.tracer(0)
 #net
 net = Turtle()
 net.pencolor("white")
@@ -35,18 +37,18 @@ comp_paddle.penup()
 comp_paddle.fillcolor("white")
 comp_paddle.pencolor("white")
 comp_paddle.shape("rectangle")
-comp_paddle.goto(900,0)
+comp_paddle.goto(900,-200)
+comp_paddle.forward(10)
+comp_paddle.seth(90)
 def move_up():
 
     user_paddle.forward(10)
     user_paddle.seth(90)
-    ball.backward(10)
 
 
 def move_down():
     user_paddle.backward(10)
     user_paddle.seth(90)
-    ball.backward(10)
 
 #create a paddle event
 screen.listen()
@@ -62,6 +64,7 @@ ball.pencolor("white")
 
 def move_ball_backward():
     ball.backward(10)
+
 #create a bouncing ball
 def move_ball_forward():
     ball.forward(10)
@@ -72,14 +75,34 @@ def is_forward_or_backward(ball_direction):
         move_ball_backward()
     else:
         move_ball_forward()
-while(True):
-    screen.onkeypress(move_up, "w")
-    screen.onkeypress(move_down, "s")
+screen.onkeypress(move_up, "w")
+screen.onkeypress(move_down, "s")
+game_is_on = True
+user_paddle_degree = [280,440]
+while(game_is_on):
+    screen.update()
+
     is_forward_or_backward(ball_direction)
     if(ball.ycor()<=user_paddle.ycor()+35 and ball.ycor()>=user_paddle.ycor()-35 and ball.xcor()<=-890):
         ball_direction = 1
-        
+        ball.seth(random.randint(user_paddle_degree[0],user_paddle_degree[1]))
+    if(ball.ycor()<=comp_paddle.ycor()+35 and ball.ycor()>=comp_paddle.ycor()-35 and ball.xcor()>=890):
+        ball_direction = 0
+        ball.seth(random.randint(user_paddle_degree[0],user_paddle_degree[1]))
+    if(ball.ycor()>500):
+        if(ball_direction==1):
+            ball.seth(315)
+        if(ball_direction==0):
+            ball.seth(45)
+
+    if(ball.ycor()<-500):
+        if(ball_direction==1):
+            ball.seth(45)
+        if(ball_direction==0):
+            print("I am test")
+            ball.seth(315)
+
+
     time.sleep(0.01)
-    print(user_paddle.pos())
 
 screen.exitonclick()
