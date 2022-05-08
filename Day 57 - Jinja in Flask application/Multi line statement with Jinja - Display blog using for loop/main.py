217691505eed5f50311127f44549d97b1939e570
supawichza40 <supawichza@gmail.com>
2022-05-08 21:58:17 +0100

    Starting dynamic html template

---
+++ Day 57 - Jinja in Flask application/Multi line statement with Jinja - Display blog using for loop/main.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template
+from markupsafe import escape
+import requests
+
+app = Flask(__name__)
+
+@app.route("/blog")
+def blog():
+    blog_response = requests.get(url="https://api.npoint.io/c790b4d5cab58020d391")
+    return render_template("posts.html",posts=blog_response.json())
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# for info in response.json():
+#     print(info)
