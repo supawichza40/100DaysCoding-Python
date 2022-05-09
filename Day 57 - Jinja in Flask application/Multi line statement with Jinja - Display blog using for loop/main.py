43a220ddf863f90923b7e257acc08b63cb3df63f
Supawichza40 <supawichza@gmail.com>
2022-05-09 08:39:12 +0100

    trying to create get and post request linking to mongodb

--- Day 57 - Jinja in Flask application/Multi line statement with Jinja - Display blog using for loop/main.py
+++ Day 57 - Jinja in Flask application/Multi line statement with Jinja - Display blog using for loop/main.py
@@ -1,13 +1,37 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request
 from markupsafe import escape
 import requests
-
+import sys
 app = Flask(__name__)
 print("Dear")
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/user/question")
+def user():
+    return render_template("user_enquiry.html")
+
+
+@app.route("/user/success", methods=["POST"])
+def user_submit():
+    print("I am user-submit")
+    print(request.form["first_name"],
+          request.form["last_name"], file=sys.stderr)
+
+    return render_template("users.html", first_name=request.form["first_name"])
+
+
 @app.route("/blog")
 def blog():
-    blog_response = requests.get(url="https://api.npoint.io/c790b4d5cab58020d391")
-    return render_template("posts.html",posts=blog_response.json())
+    blog_response = requests.get(
+        url="https://api.npoint.io/c790b4d5cab58020d391")
+    return render_template("posts.html", posts=blog_response.json())
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
